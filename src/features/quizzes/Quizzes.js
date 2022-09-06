d69791b5edf5fa4e03e2b80e91935fb25cbbe15b
SOliv1 <s.oliver1@icloud.com>
2022-09-06 10:24:45 +0100

    final commit to make cards render in quizz

--- src/features/quizzes/Quizzes.js
+++ src/features/quizzes/Quizzes.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import ROUTES from "../../app/routes";
+import { selectQuizzes } from "./quizzesSlice";
+import { useSelector } from "react-redux";
 
 export default function Quizzes() {
-  const quizzes = {}; // replace this with a call to your selector to get all the quizzes in state
+  const quizzes = useSelector(selectQuizzes); // replace this with a call to your selector to get all the quizzes in state
   return (
     <section className="center">
       <h1>Quizzes</h1>
--- src/features/topics/topicSlice.js
+++ src/features/topics/topicSlice.js
@@ -16,6 +16,7 @@ export const topicsSlice = createSlice(
 					quizIds: []
 				}
 			},
+
 			addQuizIdForTopic: (state, action) => {
 				const {quizId, topicId} = action.payload;
 				state.topics[topicId].quizIds.push(quizId);
